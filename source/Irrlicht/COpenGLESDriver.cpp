90e2699b700abdb9b79e24f9ea6a809b87c826d3
wangtianhang <690879430@qq.com>
2017-01-05 00:28:36 +0800

    部分opengles代码

--- source/Irrlicht/COpenGLESDriver.cpp
+++ source/Irrlicht/COpenGLESDriver.cpp
@@ -10,7 +10,27 @@ namespace video
 IVideoDriver* createOpenGLESDriver(const SIrrlichtCreationParameters& params,
 	io::IFileSystem* io, CIrrDeviceWin32* device)
 {
-	return NULL;
+	COpenGLESDriver* ogl =  new COpenGLESDriver(params, io, device);
+	if (!ogl->initDriver(device))
+	{
+		ogl->drop();
+		ogl = 0;
+	}
+	return ogl;
+}
+
+
+COpenGLESDriver::COpenGLESDriver( const SIrrlichtCreationParameters& params, io::IFileSystem* io, CIrrDeviceWin32* device )
+	: CNullDriver(io, params.WindowSize)
+{
+#ifdef _DEBUG
+	setDebugName("COpenGLDriver");
+#endif
+}
+
+bool COpenGLESDriver::initDriver( CIrrDeviceWin32* device )
+{
+	return false;
 }
 
 }
--- source/Irrlicht/COpenGLESDriver.h
+++ source/Irrlicht/COpenGLESDriver.h
@@ -9,4 +9,26 @@
 #include "CNullDriver.h"
 #include "IMaterialRendererServices.h"
 
+namespace irr
+{
+	class CIrrDeviceWin32;
+}
+
+namespace irr
+{
+
+namespace video
+{
+
+	class COpenGLESDriver : public CNullDriver
+	{
+	public:
+		COpenGLESDriver(const SIrrlichtCreationParameters& params, io::IFileSystem* io, CIrrDeviceWin32* device);
+		bool initDriver(CIrrDeviceWin32* device);
+	};
+
+}
+
+}
+
 #endif
