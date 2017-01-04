9e87a8178bd108d5c442ddd4bff27a78b44af3a5
wangtianhang <690879430@qq.com>
2017-01-05 00:15:19 +0800

    编译错 补提交

---
+++ source/Irrlicht/COpenGLESDriver.cpp
@@ -0,0 +1,18 @@
+
+#include "COpenGLESDriver.h"
+#include "CIrrDeviceWin32.h"
+
+namespace irr
+{
+namespace video
+{
+
+IVideoDriver* createOpenGLESDriver(const SIrrlichtCreationParameters& params,
+	io::IFileSystem* io, CIrrDeviceWin32* device)
+{
+	return NULL;
+}
+
+}
+}
+
---
+++ source/Irrlicht/COpenGLESDriver.h
@@ -0,0 +1,12 @@
+#ifndef __C_VIDEO_OPEN_GLES_H_INCLUDED__
+#define __C_VIDEO_OPEN_GLES_H_INCLUDED__
+
+#include "IrrCompileConfig.h"
+
+#include "SIrrCreationParameters.h"
+
+
+#include "CNullDriver.h"
+#include "IMaterialRendererServices.h"
+
+#endif
